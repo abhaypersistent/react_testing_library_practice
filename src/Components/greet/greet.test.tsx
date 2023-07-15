ba5d8c30b04c8684f9a9add91db7eb438910a591
Abhay Pandey <abhay_pandey@persistent.com>
2023-07-15 20:16:45 +0530

    Jest practice

--- src/Components/greet/greet.test.tsx
+++ src/Components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import {render, screen} from '@testing-library/react';
 import { Greet } from './greet';
 
-test('Great renders correctly', () => {
-    render(<Greet />);
-    const textField = screen.getByText(/hello/i);
-    expect(textField).toBeInTheDocument();
-})
-
-test('Greet renders with the name passed', () => {
-    render(<Greet name="abhay"/>);
-    const textField = screen.getByText('Hello abhay');
-    expect(textField).toBeInTheDocument();
+describe('Greet', () => {
+    it('Great renders correctly', () => {
+        render(<Greet />);
+        const textField = screen.getByText(/hello/i);
+        expect(textField).toBeInTheDocument();
+    })
+    
+    it('Greet renders with the name passed', () => {
+        render(<Greet name="abhay"/>);
+        const textField = screen.getByText('Hello abhay');
+        expect(textField).toBeInTheDocument();
+    })
 })
