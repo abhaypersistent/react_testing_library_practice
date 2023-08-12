b151d9f03bcb00db88fd9c67b31b31c4f98eaa96
Abhay Pandey <abhay_pandey@persistent.com>
2023-08-12 11:15:17 +0530

    mock example

--- src/Components/counter-two/counter-two.test.tsx
+++ src/Components/counter-two/counter-two.test.tsx
@@ -1,11 +1,32 @@
 import { render, screen} from "@testing-library/react";
 import {CounterTwo} from './counter-two';
+import user from "@testing-library/user-event"
+import { execPath } from "process";
 
-describe('CounterTwo', () => {
+describe('CounterTwo test one', () => {
     test('renders Correctly',() => {
         render(<CounterTwo count={0}/>);
         const textElement = screen.getByText("Counter Two");
         expect(textElement).toBeInTheDocument();
+
+    })
+
+    test('handlers are called', async () => {
+        user.setup();
+        const incrementHandler = jest.fn();
+        const decrementHandler = jest.fn();
+        render(<CounterTwo  count={0}
+            handleIncrement={incrementHandler}
+            handleDecrement={decrementHandler}
+            />)
+
+        const getIncrementButton = screen.getByRole('button', {name: 'Increment'});    
+        const getDecrementButton = screen.getByRole('button', {name: 'Decrement'});    
+        await user.click(getIncrementButton);    
+        await user.click(getDecrementButton);    
+        expect(incrementHandler).toHaveBeenCalledTimes(1);
+        expect(decrementHandler).toHaveBeenCalledTimes(1);
+
 
     })
 })
